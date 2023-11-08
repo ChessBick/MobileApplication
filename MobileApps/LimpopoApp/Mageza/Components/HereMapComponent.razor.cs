380fadba423a18311be57b9f23eaceb6907d6b57
Henkie Sandile Mahlangu <Sandile.Mahlangu@ktc-za.com>
2023-11-08 21:38:26 +0200

    Here maps

---
+++ MobileApps/LimpopoApp/Mageza/Components/HereMapComponent.razor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mageza.Components
+{
+    public partial class HereMapComponent
+    {
+        [Inject] IJSRuntime js { get; set; }
+
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+               var dd =  await js.InvokeAsync<object>("IsConnectedMap");
+                await js.InvokeVoidAsync("LoadAndDisplayMap");
+                StateHasChanged();  
+            }
+            //return base.OnAfterRenderAsync(firstRender);
+        }
+    }
+}
---
+++ MobileApps/LimpopoApp/Mageza/wwwroot/js/HereMaps.js
@@ -0,0 +1,24 @@
+﻿
+function IsConnectedMap()
+{
+    var platform = new H.service.Platform({
+        'apikey': 'QNKoaO1cnhu0IIFH81SxTEP11zcplDKR5SN-OByvZs4'
+    });
+    return platform;
+}
+function LoadAndDisplayMap(){
+    var platform = new H.service.Platform({
+        'apikey': 'QNKoaO1cnhu0IIFH81SxTEP11zcplDKR5SN-OByvZs4'
+    });
+
+    var defaultLayers = platform.createDefaultLayers();
+
+    // Instantiate (and display) a map object:
+    var map = new H.Map(
+        document.getElementById('mapContainer'),
+        defaultLayers.vector.normal.map,
+        {
+            zoom: 10,
+            center: { lat: 52.5, lng: 13.4 }
+        });
+}
